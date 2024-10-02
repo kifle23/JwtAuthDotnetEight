56eae62ac226f9558f6007e21c5949792ba70799
kifle23 <kiflekifle23@gmail.com>
2024-10-03 02:08:41 +0300

    Add AuthExtensions.cs for JWT authentication and authorization

---
+++ Extensions/AuthExtensions.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthDotnetEight.Extensions
+{
+    public static class AuthExtensions
+{
+    public static IServiceCollection AddJwtAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication("Bearer")
+            .AddJwtBearer(options =>
+            {
+                var secret = configuration["AppSettings:Token"];
+                Console.WriteLine($"JWT Secret: {secret}");
+                if (string.IsNullOrEmpty(secret))
+                {
+                    throw new InvalidOperationException("JWT Secret is not configured.");
+                }
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+        services.AddAuthorization();
+        return services;
+    }
+
+    public static WebApplication AddAuthMiddlewares(this WebApplication app)
+    {
+      app.UseAuthentication();
+      app.UseAuthorization();
+      return app;
+    }
+}
+
+}
