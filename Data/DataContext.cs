7e8b5d8eb885319196d748403e4efe4dd07431b9
kifle23 <kiflekifle23@gmail.com>
2024-10-02 17:36:17 +0300

    Add DataContext class with User, Role, and UserRole DbSet properties and configure their relationships in OnModelCreating method

---
+++ Data/DataContext.cs
@@ -0,0 +1,30 @@
+using JwtAuthDotnetEight.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotnetEight.Data
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserRole>()
+                .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
