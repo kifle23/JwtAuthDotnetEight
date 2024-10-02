22e0b10aa3cd473dd1279f13eafbec146611c544
kifle23 <kiflekifle23@gmail.com>
2024-10-03 00:45:22 +0300

    Add LoginDto class for handling login credentials

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,9 @@
+namespace JwtAuthDotnetEight.Dtos
+{
+    public class LoginDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
