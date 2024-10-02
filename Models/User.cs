9f92dbbbcf0189f62e706de80638fa469c6288d9
kifle23 <kiflekifle23@gmail.com>
2024-10-02 16:53:09 +0300

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace JwtAuthDotnetEight.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+
+    }
+}
