d102c0b91a8fcc100d18f295818d252f5fe08698
kifle23 <kiflekifle23@gmail.com>
2024-10-03 02:08:07 +0300

    Add SwaggerExtensions.cs for configuring Swagger documentation

---
+++ Extensions/SwaggerExtensions.cs
@@ -0,0 +1,49 @@
+using Microsoft.OpenApi.Models;
+
+namespace JwtAuthDotnetEight.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Fill in the JWT token",
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new List<string>()
+                }
+                });
+            });
+            return services;
+        }
+
+        public static WebApplication UseSwaggerDocumentation(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+
+}
