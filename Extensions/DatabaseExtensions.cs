0442dce47067f5c024ef0b08ce6b5efe7b6d7596
kifle23 <kiflekifle23@gmail.com>
2024-10-03 02:08:23 +0300

    Add DatabaseExtensions.cs for configuring the database context and seeding initial data

---
+++ Extensions/DatabaseExtensions.cs
@@ -0,0 +1,59 @@
+using JwtAuthDotnetEight.Data;
+using JwtAuthDotnetEight.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotnetEight.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DevDB")));
+            return services;
+        }
+        public static WebApplication SeedDatabase(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                if (!context.Users.Any())
+                {
+                    var adminRole = new Role { Name = "Admin" };
+                    var userRole = new Role { Name = "User" };
+
+                    context.Roles.AddRange(adminRole, userRole);
+
+                    var adminUser = new User
+                    {
+                        Username = "admin",
+                        PasswordHash = BCrypt.Net.BCrypt.HashPassword("password")
+                    };
+
+                    var normalUser = new User
+                    {
+                        Username = "user",
+                        PasswordHash = BCrypt.Net.BCrypt.HashPassword("password")
+                    };
+
+                    context.Users.AddRange(adminUser, normalUser);
+
+                    context.UserRoles.AddRange(new UserRole
+                    {
+                        User = adminUser,
+                        Role = adminRole
+                    },
+                    new UserRole
+                    {
+                        User = normalUser,
+                        Role = userRole
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+            return app;
+        }
+    }
+
+}
